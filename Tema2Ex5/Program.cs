3a974f681f9f83729d37081c8a66339fe12c53af
Gergoa123 <gergo3124@gmail.com>
2022-05-09 18:19:30 +0300

    Add project files.

---
+++ Tema2Ex5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Tema3Ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            int numar;
+            Console.WriteLine("introduceti numere");
+            numar = int.Parse(Console.ReadLine());
+            int aux = numar;
+            
+            suma = suma + numar;
+            numar = int.Parse(Console.ReadLine());
+
+            while (numar != aux)
+            {
+               
+                aux = numar;
+                suma = suma + numar;
+                numar = int.Parse(Console.ReadLine());
+
+            }
+
+            suma = suma + numar;
+            Console.WriteLine("suma numerelor este " + suma);
+        }
+    }
+    
+}
+
